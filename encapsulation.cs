84bfc0e511843d0b514639c88c547ca3d13b6c5e
Catherincathy <catherin.s.gudimelli@accenture.com>
2025-07-04 09:08:59 +0000

    day2

---
+++ encapsulation.cs
@@ -0,0 +1,28 @@
+public class Person
+{
+    private int age;
+    public int Age
+    {
+        get
+        {
+            return age;
+        }
+        set
+        {
+            age = value;
+        }
+    }
+
+}
+public class Program
+{
+    public static void Main()
+    {
+        //Person p = new Person();
+        //p.Age = 30;
+        Person p = new Person { Age = 30 };
+        Console.WriteLine("Person's Age: " + p.Age);
+
+    }
+
+}
